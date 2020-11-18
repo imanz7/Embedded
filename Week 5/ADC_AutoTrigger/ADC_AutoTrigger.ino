9fcdbcbd1dc3b4d06558e76e7bfdeebe8b311431
Iman Zahurin <73816908+imanz7@users.noreply.github.com>
2020-11-18 13:03:26 +0800

    Add files via upload

---
+++ Week 5/ADC_AutoTrigger/ADC_AutoTrigger.ino
@@ -0,0 +1,28 @@
+/*
+ * a program that continuously prints out the analog value of PC3 (A3)
+ * through the serial port. Assume the followings:
+      - The clock speed of the microcontroller is 16MHz
+      - The desired clock speed of ADC is 125 kHz.
+      - Use AVCC as analog reference.
+      - Auto-trigger mode
+ */
+void setup()
+{
+    Serial.begin(9600);
+    ADMUX = 67;        //(1<<REFS0, 1<<MUX1, 1<<MUX0) - use PC3 as analog input
+    ADCSRA = 231;     //(1<<ADEN, 1<<ADSC, 1<<ADATE, 1<<ADPS2, 1<<ADPS1, 1<<ADPS0);
+}
+
+void loop()
+{     
+      ADCSRA |= 0b01000000;
+      while(((ADCSRA) & 0b01000000))    //if ADSC is set
+      {
+          int lowbyte = ADCL;
+          int highbyte = ADCH;
+          int value = (highbyte<<8)|lowbyte;
+          Serial.println (value);
+          Serial.println(".....");
+      }
+          
+  }
